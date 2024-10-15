127e6058888e06c5f43a2307dc5df9fbd110d9c7
oozi <37178638-oozi@users.noreply.replit.com>
2024-10-15 16:13:37 +0000

    Agent query: Can you access the gallery page from the home page? Is there a link to the gallery in the navigation bar?

--- app.py
+++ app.py
@@ -3,25 +3,17 @@ import logging
 import traceback
 import boto3
 from botocore.exceptions import ClientError
-from flask import Flask, render_template, request, jsonify
-from models import db, Video
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from utils.youtube_api import get_youtube_video_info
 from werkzeug.utils import secure_filename
 from uuid import uuid4
 from urllib.parse import urlparse
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///videos.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
 
 s3_client = boto3.client(
     's3',
@@ -29,6 +21,9 @@ s3_client = boto3.client(
     aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
 )
 S3_BUCKET = os.environ.get('S3_BUCKET')
+
+# In-memory storage for fused videos (replace with a database in a production environment)
+fused_videos = []
 
 def generate_presigned_url(file_name, file_type):
     s3_key = f"user-uploads/{file_name}-{uuid4()}"
@@ -48,10 +43,9 @@ def generate_presigned_url(file_name, file_type):
 def index():
     return render_template('index.html')
 
-@app.route('/dashboard')
-def dashboard():
-    videos = Video.query.all()
-    return render_template('dashboard.html', videos=videos)
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html', fused_videos=fused_videos)
 
 @app.route('/get-upload-url', methods=['POST'])
 def get_upload_url():
@@ -108,20 +102,18 @@ def process_videos():
 
         logger.debug(f"YouTube video info: {youtube_info}")
 
-        # Save video information to database
-        new_video = Video(
-            personal_video_url=personal_video_url,
-            reference_video_url=youtube_url,
-            reference_video_title=youtube_info['title'],
-            reference_video_thumbnail=youtube_info['thumbnail']
-        )
-        db.session.add(new_video)
-        db.session.commit()
-        logger.debug("Video information saved to database")
-
         # TODO: Implement actual video fusion logic
         # For now, we'll just return the personal video URL as the fused video
         fused_video_url = personal_video_url
+
+        # Store fused video information
+        fused_videos.append({
+            'personal_video_url': personal_video_url,
+            'youtube_url': youtube_url,
+            'youtube_title': youtube_info['title'],
+            'youtube_thumbnail': youtube_info['thumbnail'],
+            'fused_video_url': fused_video_url
+        })
 
         logger.debug("Video processing completed successfully")
         return jsonify({
